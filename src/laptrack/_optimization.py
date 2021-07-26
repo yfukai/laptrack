ab60973a1ea8ca36207b9426237b818bf338a9ae
Yohsuke Fukai <ysk@yfukai.net>
2021-07-26 15:23:59 +0900

    writing to optimization

--- src/laptrack/_optimization.py
+++ src/laptrack/_optimization.py
@@ -1,16 +1,34 @@
 from typing import Tuple
-from ._typing_utils import IntArray
 
-def lap_optimization(rows : IntArray, cols : IntArray, vals : IntArray) -> Tuple[IntArray,IntArray]:
+import lap
+from scipy.sparse import lil_matrix
+from scipy.sparse.csr import csr_matrix
+
+from ._typing_utils import Int,IntArray,FloatArray
+
+def __to_lap_sparse(cost_matrix : lil_matrix) -> Tuple[Int,FloatArray,IntArray,IntArray]:
+    """convert data for lap.lapmod"""
+    n=cost_matrix.shape[0]
+    cost_matrix2=csr_matrix(cost_matrix)
+    assert cost_matrix2.has_sorted_indices
+    return n,cost_matrix2.data,cost_matrix2.indptr,cost_matrix2.indices
+
+
+def lap_optimization(cost_matrix : lil_matrix) -> Tuple[float,IntArray,IntArray]:
     """Solves the linear assignment problem of sparse matrix.
 
     Parameters
     ----------
-    rows : IntArray
-        the row indices of the cost matrix.
-    cols : IntArray
-        the column indices of the cost matrix.
-    vals : IntArray
-        the values of the cost matrix.
+    cost_matrix : lil_matrix
+        the cost matrix in scipy.sparse.lil_matrix format
+
+    Returns
+    -------
+    cost : float
+        the final estimated cost
+    xs : IntArray
+        the indices such that assigned indices are (i,x[i]) (i=0 ... )
+    ys : IntArray
+        the indices such that assigned indices are (y[j],j) (j=0 ... )
     """
-    pass
+    return lap.lapmod(*__to_lap_sparse(cost_matrix))
--- src/laptrack/tracking.py
+++ src/laptrack/tracking.py
@@ -1,15 +1,70 @@
-from typing import List, Literal, Optional, Union
+from typing import Sequence, Optional, Union, List
+from typing_extensions import Literal
 
 import numpy as np
 from numpy import typing as npt
+from scipy.spatial import distance_matrix
 
 from ._typing_utils import FloatArray, Float
+from ._cost_matrix import build_frame_cost_matrix
+from ._optimization import lap_optimization
 
-def track_points(coords : List[FloatArray], 
-                 props : Optional[FloatArray] = None,
-                 track_distance_cutoff : Optional[Float] = 15,
-                 gap_closing_cutoff : Optional[Union[Float,Literal[False]]] = 15,
-                 segment_splitting_cutoff : Optional[Union[Float,Literal[False]]] = False,
-                 segment_merging_cutoff : Optional[Union[Float,Literal[False]]] = False,
-                 ) -> List[npt.NDArray[np.uint32]]:
-    pass
+def track_points(coords : Sequence[FloatArray], 
+                 props : Optional[Sequence[FloatArray]] = None,
+                 track_distance_cutoff : Float = 15,
+                 track_start_cost : Float = 30, # b in Jaqaman et al 2008 NMeth.
+                 track_end_cost : Float = 30, # d in Jaqaman et al 2008 NMeth.
+                 gap_closing_cutoff : Union[Float,Literal[False]] = 15,
+                 segment_splitting_cutoff : Union[Float,Literal[False]] = False,
+                 no_splitting_cost : Float = 30, # d' in Jaqaman et al 2008 NMeth.
+                 segment_merging_cutoff : Union[Float,Literal[False]] = False,
+                 no_merging_cost : Float = 30, # b' in Jaqaman et al 2008 NMeth.
+                 ) -> List[npt.NDArray[np.uint32]] :
+    """Track points by solving linear assignment problem.
+
+    Parameters
+    ----------
+    coords : Sequence[FloatArray]
+        The list of coordinates of point for each frame. The array index means (sample, dimension). 
+    props : Optional[Sequence[FloatArray]], optional
+        The properties (such as intensity) of the points (optional), by default None
+    track_distance_cutoff : Float, optional
+        The distance cutoff for the connected points in the track, by default 15
+    track_start_cost : Float, optional
+        The cost for starting the track (b in Jaqaman et al 2008 NMeth), by default 30
+    track_end_cost : Float, optional
+        The cost for ending the track (d in Jaqaman et al 2008 NMeth), by default 30
+    segment_splitting_cutoff : Union[Float,Literal[False]], optional
+        The distance cutoff for the splitting points, by default 15. If False, no splitting is allowed.
+    no_splitting_cost : Float, optional
+        The cost to reject splitting, by default 30.
+    segment_merging_cutoff : Union[Float,Literal[False]], optional
+        The distance cutoff for the merging points, by default 15. If False, no merging is allowed.
+    no_merging_cost : Float, optional
+        The cost to reject merging, by default 30.
+
+    Returns
+    -------
+    track_indices : List[npt.NDArray[np.uint32]]
+        The indices of each tracks, in the same form as that of coords.
+    """
+
+    if any(list(map(lambda coord : coord.ndim!=2,coords))):
+        raise ValueError("the elements in coords must be 2-dim.")
+    coord_dim=coords[0][0].shape[1]
+    if any(list(map(lambda coord : coord.shape[1]!=coord_dim,coords))):
+        raise ValueError("the second dimension in coords must have the same size")
+    if props:
+        if len(coords) != len(props):
+            raise ValueError("the coords and props must have the same length.")
+        if any(list(map(lambda coord_prop : coord_prop[0].shape[0]!=coord_prop[1].shape[0],zip(coords,props)))):
+            raise ValueError("the number of coords and props must be the same for each frame.")
+
+    # first linking between frames 
+    for coord1, coord2 in zip(coords[:-1],coords[1:]):
+        dist_matrix = distance_matrix(coord1,coord2)
+        cost_matrix = build_frame_cost_matrix(dist_matrix,
+                        track_distance_cutoff=track_distance_cutoff,
+                        track_start_cost=track_start_cost,
+                        track_end_cost=track_end_cost)
+        _,xs,ys = lap_optimization(cost_matrix)
