fbdf2097621581cc0ec5f29b366485ebcc220d8a
Yohsuke Fukai <ysk@yfukai.net>
2021-07-26 09:44:29 +0900

    writing tracking...

--- src/laptrack/tracking.py
+++ src/laptrack/tracking.py
@@ -1,10 +1,15 @@
-from typing import Optional
+from typing import List, Literal, Optional, Union
 
+import numpy as np
+from numpy import typing as npt
 
 from ._typing_utils import FloatArray, Float
 
-def track_points(coords : FloatArray, 
+def track_points(coords : List[FloatArray], 
                  props : Optional[FloatArray] = None,
                  track_distance_cutoff : Optional[Float] = 15,
-                 ):
-	pass
+                 gap_closing_cutoff : Optional[Union[Float,Literal[False]]] = 15,
+                 segment_splitting_cutoff : Optional[Union[Float,Literal[False]]] = False,
+                 segment_merging_cutoff : Optional[Union[Float,Literal[False]]] = False,
+                 ) -> List[npt.NDArray[np.uint32]]:
+    pass
