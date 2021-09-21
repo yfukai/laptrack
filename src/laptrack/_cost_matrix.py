ab3a47463526ed2497420bf2cabfba3da3939dcd
Yohsuke Fukai <ysk@yfukai.net>
2021-09-21 17:41:02 +0900

    organized license notice for TrackMate-associated code

--- src/laptrack/_cost_matrix.py
+++ src/laptrack/_cost_matrix.py
@@ -95,8 +95,6 @@ def build_segment_cost_matrix(
     cost_matrix : Optional[FloatArray]
         the cost matrix for frame linking, None if not appropriate
     """
-    # https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/costmatrix/JaqamanSegmentCostMatrixCreator.java # noqa:
-    # https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/SparseLAPSegmentTracker.java#L148 # noqa:
     M = gap_closing_dist_matrix.shape[0]
     assert gap_closing_dist_matrix.shape[1] == M
     assert splitting_dist_matrix.shape[0] == M
@@ -116,8 +114,17 @@ def build_segment_cost_matrix(
     if len(all_data) == 0:
         return None
 
-    # https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/costmatrix/DefaultCostMatrixCreator.java#L186 # noqa :
-    # https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/TrackerKeys.java # noqa :
+    # License notice:
+    # The algorithm to assigning track_start_cost, track_end_cost, no_splitting_cost, no_merging_cost # noqa :
+    # and min_val is taken from TrackMate (link1, link2), and relicensed to BSD-3-clause
+    # with permission from Jean-Yves Tinevez (link3).
+    #   link1 https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/costmatrix/DefaultCostMatrixCreator.java#L186 # noqa :
+    #         https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/costmatrix/JaqamanSegmentCostMatrixCreator.java # noqa:
+    #         https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/sparselap/SparseLAPSegmentTracker.java#L148 # noqa:
+    #   link2 (default parameters for alternative_cost_percentile, alternative_cost_factor) # noqa :
+    #         https://github.com/fiji/TrackMate/blob/5a97426586b3c592c986c57aa1a09bab9d21419c/src/main/java/fiji/plugin/trackmate/tracking/TrackerKeys.java # noqa :
+    #   link3 https://forum.image.sc/t/linear-assignment-problem-based-tracking-package-in-python/57793 # noqa :
+
     if (
         track_start_cost is None
         or track_end_cost is None
@@ -151,5 +158,7 @@ def build_segment_cost_matrix(
         [track_start_cost, track_end_cost, no_splitting_cost, no_merging_cost]
     )
     C[inds[1] + M + N1, inds[0] + M + N2] = min_val
+
+    # End of the relicensed code
 
     return C
