0b03f9a2bbc4c3b484090977c634160ec3fc7458
Yohsuke Fukai <ysk@yfukai.net>
2022-08-02 13:47:41 +0900

    test completed

--- tests/test_data_conversion.py
+++ tests/test_data_conversion.py
@@ -28,6 +28,61 @@ def test_convert_dataframe_to_coords():
 def test_convert_tree_to_dataframe():
     tree = nx.from_edgelist(
         [
-            ((0, 0), (1, 1)),
+            ((0, 0), (1, 0)),
+            ((1, 0), (2, 0)),
+            ((2, 0), (3, 0)),
+            ((3, 0), (4, 0)),
+            ((4, 0), (5, 0)),
+            ((2, 0), (3, 1)),
+            ((3, 1), (4, 1)),
+            ((4, 1), (5, 1)),
+            ((1, 2), (2, 2)),
+            ((2, 2), (3, 2)),
+            ((3, 2), (4, 2)),
+            ((1, 3), (2, 2)),
         ]
+    )
+    segments = [
+        [(0, 0), (1, 0), (2, 0)],
+        [(3, 0), (4, 0), (5, 0)],
+        [(3, 1), (4, 1), (5, 1)],
+        [(1, 2)],
+        [(2, 2), (3, 2), (4, 2)],
+        [(1, 3)],
+    ]
+    clones = [segments[:3], segments[3:]]
+    # 0-0-0-0-0-0
+    #      |
+    #      -1-1-1
+    #   2-2-2-2
+    #    |
+    #   3-
+    df, split_df, merge_df = data_conversion.convert_tree_to_dataframe(tree)
+    len(set(df["track_id"])) == len(segments)
+    segment_ids = []
+    for segment in segments:
+        len(set(df.loc[segment, "track_id"])) == 1  # unique track id
+        segment_ids.append(df.loc[segment, "track_id"].iloc[0])
+    for clone in clones:
+        clone_all = sum(clone, [])
+        len(set(df.loc[clone_all, "tree_id"])) == 1  # unique track id
+
+    split_df_target = np.array(
+        [
+            [segment_ids[0], segment_ids[1]],
+            [segment_ids[0], segment_ids[2]],
+        ]
+    )
+    assert np.all(
+        split_df[["parent_track_id", "child_track_id"]].values == split_df_target
+    )
+
+    merge_df_target = np.array(
+        [
+            [segment_ids[3], segment_ids[4]],
+            [segment_ids[5], segment_ids[4]],
+        ]
+    )
+    assert np.all(
+        merge_df[["parent_track_id", "child_track_id"]].values == merge_df_target
     )
