e9434b9ef0e10c5e71cfba5495580580a6b55ba3
Yohsuke Fukai <ysk@yfukai.net>
2022-08-05 18:00:17 +0900

    updated docs

--- noxfile.py
+++ noxfile.py
@@ -122,7 +122,13 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "sphinx-rtd-theme")
+    session.install(
+        "sphinx",
+        "sphinx-autobuild",
+        "sphinx-click",
+        "sphinx-rtd-theme",
+        "autodoc_pydantic",
+    )
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
