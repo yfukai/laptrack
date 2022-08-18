80daecdd9c70f19f26601ade6f533038be481968
Yohsuke Fukai <ysk@yfukai.net>
2022-08-18 14:28:03 +0900

    test running ...

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -63,8 +63,8 @@ def test_scores_exclude(test_trees) -> None:
         "union_ratio": 6 / 10,
         "true_ratio": 6 / 9,
         "predicted_ratio": 6 / 7,
-        "track_purity": 7 / 9,
-        "target_effectiveness": 6 / 11,
+        "track_purity": 5 / 7,
+        "target_effectiveness": 6 / 9,
         "division_recovery": -1,
     }
     assert score == calc_scores(true_tree.edges, pred_tree.edges, exclude_edges)
@@ -79,8 +79,8 @@ def test_scores_exclude2(test_trees) -> None:
         "union_ratio": 7 / 11,
         "true_ratio": 7 / 10,
         "predicted_ratio": 7 / 8,
-        "track_purity": 7 / 9,
-        "target_effectiveness": 6 / 11,
+        "track_purity": 6 / 8,
+        "target_effectiveness": 6 / 10,
         "division_recovery": 1,
     }
     assert score == calc_scores(true_tree.edges, pred_tree.edges, exclude_edges)
@@ -91,13 +91,13 @@ def test_scores_include_frames(test_trees) -> None:
     exclude_edges = [
         ((2, 0), (3, 0)),
     ]
-    include_frames = [0, 2, 4, 5]
+    include_frames = [0, 2, 3, 4]
     score = {
-        "union_ratio": 7 / 11,
-        "true_ratio": 7 / 10,
-        "predicted_ratio": 7 / 8,
-        "track_purity": 7 / 9,
-        "target_effectiveness": 6 / 11,
+        "union_ratio": 5 / 9,
+        "true_ratio": 5 / 8,
+        "predicted_ratio": 5 / 6,
+        "track_purity": 4 / 6,
+        "target_effectiveness": 4 / 8,
         "division_recovery": 1,
     }
     assert score == calc_scores(
