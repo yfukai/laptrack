3ac3723a5bffb93f1ba9607c8c14d1eb27852eb6
Yohsuke Fukai <ysk@yfukai.net>
2022-11-06 14:11:28 +0900

    update test

--- tests/test_metric_utils.py
+++ tests/test_metric_utils.py
@@ -13,22 +13,25 @@ def test_label_overlap() -> None:
             [[[0, 1, 1, 1, 0], [5, 5, 5, 5, 5]], [[0, 1, 1, 1, 0], [0, 1, 1, 1, 0]]],
         ]
     )
-    lo = LabelOverlap(labels)
-    frame_labels = [np.unique(label) for label in labels]
-    frame_labels = [x[x > 0] for x in frame_labels]
-    for f1, f2 in [(0, 0), (0, 1), (1, 2)]:
-        for l1, l2 in product(frame_labels[f1], frame_labels[f2]):
-            b1 = labels[f1] == l1
-            b2 = labels[f2] == l2
+    labelss = [labels, list(labels)]
 
-            intersect = np.sum(b1 & b2)
-            union = np.sum(b1 | b2)
-            r1 = np.sum(b1)
-            r2 = np.sum(b2)
-            res = lo.calc_overlap(f1, l1, f2, l2)
-            assert (
-                intersect,
-                (intersect / union),
-                (intersect / r1),
-                (intersect / r2),
-            ) == res
+    for labels in labelss:
+        lo = LabelOverlap(labels)
+        frame_labels = [np.unique(label) for label in labels]
+        frame_labels = [x[x > 0] for x in frame_labels]
+        for f1, f2 in [(0, 0), (0, 1), (1, 2)]:
+            for l1, l2 in product(frame_labels[f1], frame_labels[f2]):
+                b1 = labels[f1] == l1
+                b2 = labels[f2] == l2
+
+                intersect = np.sum(b1 & b2)
+                union = np.sum(b1 | b2)
+                r1 = np.sum(b1)
+                r2 = np.sum(b2)
+                res = lo.calc_overlap(f1, l1, f2, l2)
+                assert (
+                    intersect,
+                    (intersect / union),
+                    (intersect / r1),
+                    (intersect / r2),
+                ) == res
