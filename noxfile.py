b6f59a05a58aa8e5e5df15433de050511890d728
Yohsuke Fukai <ysk@yfukai.net>
2023-04-02 22:35:33 +0900

    updated python versions

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 package = "laptrack"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.11", "3.10", "3.9", "3.8"]
 safety_ignore = [44717, 44715, 44716, 51457]  # ignore numpy 1.21 CVEs and py 1.11.0
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
