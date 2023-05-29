46480b80dfa06e27c06b4554fd54bf24664d709b
Yohsuke Fukai <ysk@yfukai.net>
2023-05-29 13:41:39 +0900

    updating dataset

--- src/laptrack/datasets.py
+++ src/laptrack/datasets.py
@@ -26,9 +26,12 @@ TEST_DATA_PROPS = {
         "filename": "overlap_tracking_data/labels.npy",
         "hash": "md5:a1b12b0d08c894b804d5440010cef77e",
     },
+    "mouse_3D_embryo": {},  # 10.1038/s41540-020-00152-8
+    # https://bbbc.broadinstitute.org/BBBC050
 }
 
-
+# "We used image set BBBC050 [Tokuoka, Yuta, et al. npj Syst Biol Appl 6, 32 (2020)], available from the Broad Bioimage Benchmark Collection [Ljosa et al., Nature Methods, 2012]."
+#  The images and ground truth are licensed under a Creative Commons Attribution 3.0 Unported License (Commercial use allowed) by Akira Funahashi.
 POOCH = pooch.create(
     path=pooch.os_cache("laptrack"),
     # Use the Zenodo DOI
